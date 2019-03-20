51270dc6250a6883a0f358098a7ef89459c04eca
larryvsdata <larryvsdata@gmail.com>
2019-03-20 15:28:41 -0700

    Commit2-Some Migrations

--- first_app/admin.py
+++ first_app/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from first_app.models import AccessRecord , Topic , WebPage
+
+admin.site.register(AccessRecord)
+admin.site.register(Topic)
+admin.site.register(WebPage)
 
 # Register your models here.
--- first_app/models.py
+++ first_app/models.py
@@ -9,3 +9,14 @@ class Topic(models.Model):
 class WebPage(models.Model):
     topic = models.ForeignKey(Topic)
     name = models.CharField(max_length = 264 , unique = True)
+    url = models.URLField(unique=True)
+
+    def __str__(self):
+        return self.name
+
+class AccessRecord(models.Model):
+    name= models.ForeignKey(WebPage)
+    date = models.DateField()
+
+    def __str__(self):
+        return str(self.date)
